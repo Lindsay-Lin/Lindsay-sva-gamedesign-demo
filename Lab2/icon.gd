b097824532e66b0446be101040772df1d612b680
Lindsay-Lin <slin22@sva.edu>
2026-02-23 03:38:15 -0500

    Lab 2

---
+++ Lab2/icon.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+@export var speed = 2.0
+
+@export var my_name = ""
+
+@export var moving = false
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	print("Hello World!")
+	print(position.x)
+
+#func _input(event: InputEvent) -> void:
+	#print(event)
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(Input.is_action_just_pressed("ui_accept"))
+	position.x += speed
+	position.y += speed
+	
+	pass
